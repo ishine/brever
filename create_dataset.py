3fbee372260e4fa59b4fcb32b77c8c1479254868
philgzl <services@philgzl.com>
2020-02-12 16:18:53 +0100

    Features can now be selected using their names in the features module. Time spent on each feature displayed.

--- create_dataset.py
+++ create_dataset.py
@@ -13,6 +13,9 @@ import brever.features
 import brever.mixture
 
 
+# set random state for reproducibility
+random.seed(0)
+
 # output dataset path
 output_path = 'data/datasets/temp.h5'
 
@@ -53,12 +56,16 @@ assert hrir_fs == fs
 
 # mixture parameters
 snrs = range(-5, 16)
-n_mixtures = 10
+n_mixtures = 1
 padding = round(0*fs)
+
+# features to calculate
+feature_list = ['ild', 'itd', 'ic']
 
 # main loop
 features = []
 labels = []
+times_spent = np.zeros(len(feature_list))
 for i in range(n_mixtures):
     sys.stdout.write('\rProcessing mixture %i/%i...' % (i+1, n_mixtures))
     sentence = random.choice(sentences)
@@ -68,13 +75,22 @@ for i in range(n_mixtures):
                                                 snr, padding)
     # extract features
     mixture_filt, _ = brever.gammatone_filt(mix)
-    ITD = brever.features.itd(mixture_filt)
-    ILD = brever.features.ild(mixture_filt)
-    IC = brever.features.ic(mixture_filt)
-    features.append(np.hstack((ITD, ILD, IC)))
+    features_mix = []
+    for i, feature_name in enumerate(feature_list):
+        t = time.time()
+        feature_func = getattr(brever.features, feature_name)
+        features_mix.append(feature_func(mixture_filt))
+        times_spent[i] += time.time() - t
+    features.append(np.hstack(features_mix))
+
     # extract labels
     IRM = brever.irm(target, noise)
     labels.append(IRM)
+print('')
+
+# print time spent
+for feature_name, time_spent in zip(feature_list, times_spent):
+    print('Time spent calculating "%s": %.2f' % (feature_name, time_spent))
 
 # concatenate features and labels
 features = np.vstack(features)
