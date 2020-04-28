8265e01cdcd2f902ea3a170e323d7401bb1f4f5d
philgzl <services@philgzl.com>
2020-04-28 10:47:00 +0200

    Updated misleading comments in config.py

--- brever/config.py
+++ brever/config.py
@@ -131,7 +131,8 @@ def defaults():
     config.POST.LOAD = False
     config.POST.FEATURES = {
         'ild',
-        # 'itd_ic',
+        # 'itd',
+        # 'ic',
         'mfcc',
         # 'pdf',
     }
