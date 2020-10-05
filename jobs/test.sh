7cf525102c79275ed754cc91ff038cb7ac491c13
philgzl <services@philgzl.com>
2020-10-05 12:17:20 +0200

    Update test bash script

--- jobs/test.sh
+++ jobs/test.sh
@@ -5,19 +5,32 @@ usage() { echo "Usage: $0 [inputs] [-f] [--njobs <int>]" 1>&2; exit 1; }
 FORCE=''
 NJOBS=1
 
-while getopts ':f-:' optchar
+while true
 do
-    case ${optchar} in
-        -)
-            case "${OPTARG}" in
-                njobs)
-                    echo 123
-                    NJOBS="${!OPTIND}"; OPTIND=$(($OPTIND+1)) ;;
-                *) usage ;;
-            esac;;
-        f) FORCE='-f' ;;
-        *) usage ;;
-    esac
+    while getopts ':f-:' optchar
+    do
+        case ${optchar} in
+            -)
+                case "${OPTARG}" in
+                    njobs)
+                        echo 123
+                        NJOBS="${!OPTIND}"; OPTIND=$(($OPTIND+1)) ;;
+                    *) usage ;;
+                esac ;;
+            f) FORCE='-f' ;;
+            *) usage ;;
+        esac
+    done
+
+    while [[ $OPTIND <= $# && ${!OPTIND} != -* ]]
+    do
+        ((OPTIND++))
+    done
+    if [[ $OPTIND > $# ]]
+    then
+        break
+    fi
+
 done
 
 shift "$((OPTIND-1))"
