0fe2ee9f996ed643d5643c6173e6a468217c4eac
philgzl <services@philgzl.com>
2020-10-05 16:56:18 +0200

    Update model testing bash script

--- jobs/test_model.sh
+++ jobs/test_model.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
-source jobs/argparse.sh
+source jobs/parse_args.sh
+
+function parse_yaml {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+eval $(parse_yaml defaults.yaml)
+
 for input in "$@"
 do
-    bash jobs/sender.sh jobs/job.sh "python scripts/test_model.py $input"
-    matlab -nodisplay -r "addpath matlab; testModel $input"
+    bash jobs/send.sh jobs/job.sh "python scripts/test_model.py $input"
+    matlab -nodisplay -r "addpath matlab; testModel $input $PRE_FS $PRE_MIXTURES_PADDING"
 done
