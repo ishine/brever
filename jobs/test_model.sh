44a0d27c5ec2c489c67451781eb894d9dd767439
philgzl <services@philgzl.com>
2020-10-06 12:54:04 +0200

    Call matlab in job command

--- jobs/test_model.sh
+++ jobs/test_model.sh
@@ -22,6 +22,5 @@ eval $(parse_yaml defaults.yaml)
 
 for input in "$@"
 do
-    bash jobs/send.sh jobs/job.sh "python scripts/test_model.py $input"
-    matlab -nodisplay -r "addpath matlab; testModel $input $PRE_FS $PRE_MIXTURES_PADDING"
+    bash jobs/send.sh jobs/job.sh 'python scripts/test_model.py '"$input"'; matlab -nodisplay -nodesktop -nosplash -r "addpath matlab; addpath matlab/loizou; testModel '"$input $PRE_FS $PRE_MIXTURES_PADDING"'"'
 done
