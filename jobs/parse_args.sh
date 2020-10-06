65083577cdf4412a84534ff0c0c19431e24f25a1
philgzl <services@philgzl.com>
2020-10-06 15:24:03 +0200

    Use the force argument when submitting jobs

--- jobs/parse_args.sh
+++ jobs/parse_args.sh
@@ -4,12 +4,12 @@ OPTS=`getopt -o fn: -l force,njobs: -- "$@"`
 if [ $? -ne 0 ]; then exit 1; fi
 eval set -- "$OPTS"
 
-FORCE=false
+FORCE=""
 NJOBS=1
 
 while true; do
   case "$1" in
-    -f | --force ) FORCE=true; shift ;;
+    -f | --force ) FORCE=" -f"; shift ;;
     -n | --njobs ) NJOBS="$2"; shift; shift ;;
     -- ) shift; break ;;
   esac
--- jobs/send.sh
+++ jobs/send.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+echo submitting "$1" containing the following command: "$2"
 sed -e "s|command|$2|g" < $1 | bsub
