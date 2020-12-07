1557649500b45ae42733445cf07a9f44e43ce715
philgzl <services@philgzl.com>
2020-12-07 14:10:50 +0100

    Force test dataset init script to take alias argument

--- scripts/initialize_testing_datasets.py
+++ scripts/initialize_testing_datasets.py
@@ -7,7 +7,7 @@ import yaml
 from brever.modelmanagement import set_dict_field
 
 
-def main(force, dirpath_target):
+def main(alias, force, dirpath_target):
     base_config = {
         'PRE': {
             'MIXTURES': {
@@ -69,7 +69,10 @@ def main(force, dirpath_target):
                 )
 
             letter = room_alias[-1].upper()
-            dirname = f'testing_snr{snr}_room{letter}'
+            if alias == '':
+                dirname = f'testing_snr{snr}_room{letter}'
+            else:
+                dirname = f'testing_{alias}_snr{snr}_room{letter}'
             dirpath = os.path.join('data', 'processed', dirname)
             if not os.path.exists(dirpath):
                 os.mkdir(dirpath)
@@ -84,9 +87,11 @@ def main(force, dirpath_target):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='initialize testing datasets')
+    parser.add_argument('alias',
+                        help='dataset alias')
     parser.add_argument('-f', '--force', action='store_true',
                         help='overwrite config file if already exists')
     parser.add_argument('--dirpath-target', '--force', action='store_true',
                         help='path to target speech database')
     args = parser.parse_args()
-    main(args.force, args.dirpath_target)
+    main(args.alias, args.force, args.dirpath_target)
