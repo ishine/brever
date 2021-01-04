734c3d7ae831b5b0e81c0a61f529d56af7868469
philgzl <services@philgzl.com>
2021-01-04 11:45:13 +0100

    Add option to set field in find model script

--- scripts/find_model.py
+++ scripts/find_model.py
@@ -1,10 +1,17 @@
 import os
 import shutil
 
-from brever.modelmanagement import find_model, ModelFilterArgParser
+import yaml
+
+from brever.config import defaults
+from brever.modelmanagement import (find_model, ModelFilterArgParser,
+                                    set_config_field)
 
 
-def main(delete=False, **kwargs):
+def main(delete=False, set_field=None, **kwargs):
+    if delete and set_field:
+        raise ValueError('Can\'t use both --delete and --set-field')
+
     models = find_model(**kwargs)
 
     trained = []
@@ -27,7 +34,7 @@ def main(delete=False, **kwargs):
         print(model_id)
 
     if models and delete:
-        print(f'{len(models)} will be deleted.')
+        print(f'{len(models)} models will be deleted.')
         resp = input('Do you want to continue? y/n')
         if resp == 'y':
             for model_id in models:
@@ -37,10 +44,40 @@ def main(delete=False, **kwargs):
         else:
             print('No model was deleted')
 
+    if models and set_field:
+        tag, value = set_field
+        tag = tag.replace('_', '-')
+        parser = ModelFilterArgParser()
+        args, _ = parser.parse_args([f'--{tag}', value])
+        tag = tag.replace('-', '_')
+        value, = getattr(args, tag)
+        print((f'{len(models)} models will have their {tag} field set to '
+               f'{value}.'))
+        resp = input('Do you want to continue? y/n')
+        if resp == 'y':
+            for model_id in models:
+                if model_id in trained:
+                    filenames = ['config.yaml', 'config_full.yaml']
+                else:
+                    filenames = ['config.yaml']
+                for filename in filenames:
+                    config_path = os.path.join('models', model_id, filename)
+                    with open(config_path, 'r') as f:
+                        config = yaml.safe_load(f)
+                    set_config_field(config, tag, value)
+                    defaults().update(config)  # throw error if conf not valid
+                    with open(config_path, 'w') as f:
+                        yaml.dump(config, f)
+        else:
+            print('No model was updated')
+
 
 if __name__ == '__main__':
     parser = ModelFilterArgParser(description='find models')
     parser.add_argument('-d', '--delete', action='store_true',
                         help='delete found models')
+    parser.add_argument('--set-field', nargs=2,
+                        help='set a field in config files (use carefully)')
     filter_args, extra_args = parser.parse_args()
-    main(delete=extra_args.delete, **vars(filter_args))
+    main(delete=extra_args.delete, set_field=extra_args.set_field,
+         **vars(filter_args))
