71faa61e93dcba1b9d8cf368da8beb282cda90f7
philgzl <services@philgzl.com>
2021-03-03 17:10:01 +0100

    Update job parameters

--- jobs/job.sh
+++ jobs/job.sh
@@ -5,13 +5,13 @@
 ### -- set the job Name --
 #BSUB -J jobname
 ### -- ask for number of cores (default: 1) --
-#BSUB -n 1
+#BSUB -n 8
 ### -- Select the resources: 1 gpu in exclusive process mode --
 #BSUB -gpu "num=1:mode=exclusive_process"
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
 #BSUB -W 24:00
-# request 8GB of system-memory
-#BSUB -R "rusage[mem=8GB]"
+# request 16GB of system-memory
+#BSUB -R "rusage[mem=16GB]"
 ### -- Specify the output and error file. %J is the job-id --
 ### -- -o and -e mean append, -oo and -eo mean overwrite --
 #BSUB -oo jobs/logs/%J.out
