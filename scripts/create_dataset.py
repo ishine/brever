adcebd07a79ca175fb30be3a1ee854a2007ef375
philgzl <services@philgzl.com>
2021-03-05 21:47:43 +0100

    Fix mixture indices in dataset not being saved correctly

--- scripts/create_dataset.py
+++ scripts/create_dataset.py
@@ -223,10 +223,10 @@ def main(dataset_dir, force):
 
         # apply label and reverse filter to obtain oracle signals
         if config.PRE.MIXTURES.SAVE:
-            for (i_start, i_end), label in zip(labelExtractor.indices,
+            for (i_start_, i_end_), label in zip(labelExtractor.indices,
                                                labelExtractor.labels):
                 mixOracle = copy.deepcopy(mixCopy)
-                mask = label_mat[:, i_start:i_end]
+                mask = label_mat[:, i_start_:i_end_]
                 mask = wola(mask, trim=len(mixOracle))
                 mask = mask[:, :, np.newaxis]
                 mixOracle.transform(partial(np.multiply, mask))
