121fbc6fe0039da68c72888b456bb84374cc961e
philgzl <services@philgzl.com>
2021-06-23 10:23:36 +0200

    Update test on jobs

--- jobs/test_model.sh
+++ jobs/test_model.sh
@@ -3,7 +3,7 @@ source jobs/parse_args.sh
 
 for input in "$@"
 do
-    if [ -f $input/scores.mat ] && [ "$FORCE" == "" ]
+    if [ -f $input/scores.yaml ] && [ "$FORCE" == "" ]
     then
         echo "model already tested: $input "
     else
--- jobs/train_and_test_model.sh
+++ jobs/train_and_test_model.sh
@@ -3,9 +3,9 @@ source jobs/parse_args.sh
 
 for input in "$@"
 do
-    if [ -f $input/scores.mat ] && [ "$FORCE" == "" ]
+    if [ -f $input/scores.yaml ] && [ "$FORCE" == "" ]
     then
-        echo "model already trained: $input "
+        echo "model already tested: $input "
     else
         bash jobs/send.sh jobs/job.sh "python scripts/train_model.py $input$FORCE; python scripts/test_model.py $input$FORCE"
     fi
