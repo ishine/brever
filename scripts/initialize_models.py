c8b7b5b78015a36095c60c0db488e38ab0fb7eab
philgzl <services@philgzl.com>
2021-06-24 10:48:03 +0200

    Ask user if to make all path combinations

--- scripts/initialize_models.py
+++ scripts/initialize_models.py
@@ -22,26 +22,7 @@ def check_if_path_exists(configs, path_type, def_cfg):
             msg = f'The specified {path_type} path does not exist'
         if not os.path.exists(path):
             print(msg)
-            resp = input('Do you wish to continue? y/n')
-            if resp == 'y':
-                return True
-            else:
-                return False
-    return True
-
-
-def check_trailing_slashes(configs, path_type):
-    if path_type not in ['train', 'val']:
-        raise ValueError('path_type must be train or val')
-    for config in configs:
-        path = bmm.get_config_field(config, f'{path_type}_path')
-        if path is not None and not path.endswith(('\\', '/')):
-            print(f'The specified {path_type} path has no trailing slashes')
-            resp = input('Do you wish to continue? y/n')
-            if resp == 'y':
-                return True
-            else:
-                return False
+            return ask_user_yes_no('Do you wish to continue? y/n')
     return True
 
 
@@ -58,11 +39,7 @@ def check_if_test_datasets_exist(configs, def_cfg):
         paths = bmm.globbed(paths)
         if not paths or any(not os.path.exists(path) for path in paths):
             print(msg)
-            resp = input('Do you wish to continue? y/n')
-            if resp == 'y':
-                return True
-            else:
-                return False
+            return ask_user_yes_no('Do you wish to continue? y/n')
     return True
 
 
@@ -70,19 +47,48 @@ def check_paths(configs, def_cfg):
     return (
         check_if_path_exists(configs, 'train', def_cfg)
         and check_if_path_exists(configs, 'val', def_cfg)
-        and check_trailing_slashes(configs, 'train')
-        and check_trailing_slashes(configs, 'val')
         and check_if_test_datasets_exist(configs, def_cfg)
     )
 
 
+def ask_user_yes_no(msg):
+    resp = None
+    while resp not in ['y', 'Y', 'n', 'N']:
+        resp = input(msg)
+        if resp in ['y', 'Y']:
+            return True
+        elif resp in ['n', 'N']:
+            return False
+        else:
+            print('Could not interpret answer')
+
+
 def main(args):
+    # ask if all train and val path combinations should be done
+    combine_paths = True
+    if args.train_path is not None and args.val_path is not None:
+        n_train = len(args.train_path)
+        n_val = len(args.val_path)
+        if n_train == n_val and n_train != 1:
+            n_combis = n_train*n_val
+            msg = ('Same number of train and val paths detected. Do you wish '
+                   'to initialize all pair combinations, or only as many '
+                   f'pairs as paths specified? [Y to do all {n_combis} '
+                   f'combinations / N to do only {n_train} pairs]')
+            combine_paths = ask_user_yes_no(msg)
+
+    # create dict of parameters to combine
     to_combine = {}
     for key in bmm.ModelFilterArgParser.arg_to_keys_map.keys():
+        if key in ['train_path', 'val_path'] and not combine_paths:
+            continue
         value = args.__getattribute__(key)
         if value is not None:
             bmm.set_config_field(to_combine, key, value)
+    if not combine_paths:
+        bmm.set_dict_field(to_combine, ['path_index'], list(range(n_train)))
 
+    # make combinations
     if to_combine:
         to_combine = bmm.flatten(to_combine)
         keys, values = zip(*to_combine.items())
@@ -90,9 +96,20 @@ def main(args):
     else:
         configs = [{}]
 
+    # add train and val paths if user requested to not make all combinations
+    if not combine_paths:
+        for config in configs:
+            path_index = config['path_index']
+            train_path = args.train_path[path_index]
+            val_path = args.val_path[path_index]
+            bmm.set_config_field(config, 'train_path', train_path)
+            bmm.set_config_field(config, 'val_path', val_path)
+            config.pop('path_index')
+
     def_cfg = defaults()
     models_dir = def_cfg.PATH.MODELS
 
+    # check if paths exist
     result = check_paths(configs, def_cfg.to_dict())
     if not result:
         print('Aborting')
@@ -100,28 +117,43 @@ def main(args):
 
     new_configs = []
     skipped = 0
+    dupes = 0
+    exists = 0
+
     for config in configs:
-        unique_id = bmm.get_unique_id(config)
-        if not os.path.exists(models_dir):
-            os.mkdir(models_dir)
-        if unique_id not in os.listdir(models_dir):
-            def_cfg.update(config)  # throws an error if config is not valid
-            uni_feats = bmm.get_config_field(config, 'uni_norm_features', None)
-            features = bmm.get_config_field(config, 'features', None)
-            if (uni_feats is not None and features is not None
-                    and not uni_feats.issubset(features)):
-                skipped += 1
-            else:
-                new_configs.append(config)
+        def_cfg.update(config)  # throws an error if config is not valid
+
+        model_id = bmm.get_unique_id(config)
+        model_dir = os.path.join(models_dir, model_id)
+
+        if os.path.exists(model_dir):
+            exists += 1
+            continue
+
+        # exclude configs with uniform normalization features not included
+        # in the list of features
+        uni_feats = bmm.get_config_field(config, 'uni_norm_features', None)
+        features = bmm.get_config_field(config, 'features', None)
+        if (uni_feats is not None and features is not None
+                and not uni_feats.issubset(features)):
+            skipped += 1
+            continue
+
+        if config not in new_configs:
+            new_configs.append(config)
+        else:
+            dupes += 1
 
     print(f'{len(configs)-skipped} config(s) attempted to be initialized.')
-    print(f'{len(configs)-len(new_configs)-skipped} already exist.')
+    print(f'{exists} already exist.')
+    print(f'{dupes} are duplicates.')
 
     if not new_configs:
         print(f'{len(new_configs)} will be initialized.')
     else:
-        resp = input(f'{len(new_configs)} will be initialized. Continue? y/n')
-        if resp == 'y':
+        msg = f'{len(new_configs)} will be initialized. Continue? y/n'
+        proceed = ask_user_yes_no(msg)
+        if proceed:
             for config in new_configs:
                 unique_id = bmm.get_unique_id(config)
                 dirpath = os.path.join(models_dir, unique_id)
