0e4029dac17387b9d6dacaa4f7af818100e22f3d
philgzl <services@philgzl.com>
2021-06-28 15:59:26 +0200

    Add yes-to-all option in sanity check script

--- scripts/check_sanity.py
+++ scripts/check_sanity.py
@@ -7,6 +7,7 @@ import brever.modelmanagement as bmm
 def main():
     models_dir = defaults().PATH.MODELS
     sane = True
+    yes_to_all = False
 
     for model_id in os.listdir(models_dir):
 
@@ -41,13 +42,24 @@ def main():
         if new_id != model_id:
             print(f'Model {model_id} has wrong ID!')
             sane = False
-            resp = input('Would you like to rename it? y/n')
-            if resp == 'y':
-                new_dir = os.path.join(models_dir, new_id)
-                os.rename(model_dir, new_dir)
-                print(f'Renamed model {model_id} to {new_id}')
-            else:
-                print('Model was not renamed')
+            while True:
+                if yes_to_all:
+                    r = 'y'
+                else:
+                    r = input('Would you like to rename it? '
+                              '[y for yes/n for no/ yes-all for yes for all]')
+                if r.lower() in ['y', 'yes-all']:
+                    if r.lower() == 'yes-all':
+                        yes_to_all = True
+                    new_dir = os.path.join(models_dir, new_id)
+                    os.rename(model_dir, new_dir)
+                    print(f'Renamed model {model_id} to {new_id}')
+                    break
+                elif r.lower() == 'n':
+                    print('Model was not renamed')
+                    break
+                else:
+                    print('Could not interpret answer')
 
     if sane:
         print('Models directory is sane')
