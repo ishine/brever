d22d5c9ca8ae0ef2cbb8912dc0990c5e219985ee
philgzl <services@philgzl.com>
2021-06-28 15:47:13 +0200

    Update sanity check script

--- scripts/check_sanity.py
+++ scripts/check_sanity.py
@@ -1,23 +1,10 @@
 import os
-import shutil
 
 from brever.config import defaults
 import brever.modelmanagement as bmm
 
 
-def format_slashes(x):
-    x = x.replace('\\', '/')
-    x = x.strip('/')
-    return x
-
-
-def shorten(long_str):
-    return long_str[:6] + '...' + long_str[-6:]
-
-
 def main():
-    full_configs = []
-    full_configs_ids = []
     models_dir = defaults().PATH.MODELS
     sane = True
 
@@ -31,84 +18,34 @@ def main():
         val_path = bmm.get_config_field(config, 'val_path')
 
         if train_path is not None and val_path is not None:
-            train_path = format_slashes(train_path)
-            val_path = format_slashes(val_path)
-            train_basename = os.path.basename(train_path)
-            val_basename = os.path.basename(val_path)
-            if not train_basename.startswith('train'):
-                print(f'Model {shorten(model_id)}\'s train basename does not '
-                      f'start with "train"! Got {train_path}')
+            if not os.path.dirname(train_path).endswith('train'):
+                print(f'Model {model_id} train path does not point to the '
+                      f'train dir! Got {train_path}')
                 sane = False
-            if not val_basename.startswith('val'):
-                print(f'Model {shorten(model_id)}\'s val basename does not '
-                      f'start with "val"! Got {val_path}')
+            if not os.path.dirname(val_path).endswith('val'):
+                print(f'Model {model_id} val path does not point to the '
+                      f'val dir! Got {val_path}')
                 sane = False
-            train_alias = train_path.replace(train_basename, '')
-            val_alias = val_path.replace(val_basename, '')
-            if train_alias != val_alias:
-                print(f'Model {shorten(model_id)}\'s train and val paths are '
-                      f'unconsistent! Got basenames {train_basename} and '
-                      f'{val_basename}')
+            if os.path.basename(train_path) != os.path.basename(val_path):
+                print(f'Model {model_id} train and val paths are not '
+                      f'consistent! Got {train_path} and {val_path}')
                 sane = False
         elif train_path is not None:
-            print(f'Model {shorten(model_id)} has a train path but no val '
-                  'path!')
+            print(f'Model {model_id} has a train path but no val path!')
             sane = False
         elif val_path is not None:
-            print(f'Model {shorten(model_id)} has a val path but no train '
-                  'path!')
+            print(f'Model {model_id} has a val path but no train path!')
             sane = False
-
-        full_config = defaults()
-        full_config.update(config)
-        full_config = full_config.to_dict()
-        if full_config in full_configs:
-            index = full_configs.index(full_config)
-            duple_id = full_configs_ids[index]
-            duple_dir = os.path.join(models_dir, duple_id)
-            print(f'Models {shorten(model_id)} and {shorten(duple_id)} '
-                  f'are duplicates!')
-            sane = False
-            scores = os.path.join(model_dir, 'scores.mat')
-            scores_dup = os.path.join(duple_dir, 'scores.mat')
-            if not os.path.exists(scores) or os.path.exists(scores_dup):
-                if not os.path.exists(scores):
-                    print(f'Model {shorten(model_id)} is untrained')
-                elif os.path.exists(scores_dup):
-                    print(f'Models {shorten(model_id)} and '
-                          f'{shorten(duple_id)} are both trained')
-                resp = input(f'Would you like to delete model '
-                             f'{shorten(model_id)}? y/n')
-                if resp == 'y':
-                    shutil.rmtree(model_dir)
-                    print(f'Deleted {shorten(model_id)}')
-                    continue
-                else:
-                    print('Model was not deleted')
-            else:
-                print(f'Model {shorten(duple_id)} is untrained')
-                resp = input(f'Would you like to delete model '
-                             f'{shorten(duple_id)}? y/n')
-                if resp == 'y':
-                    shutil.rmtree(duple_dir)
-                    print(f'Deleted model {shorten(duple_id)}')
-                    full_configs[index] = full_config
-                    full_configs_ids[index] = model_id
-                else:
-                    print('Model was not deleted')
-        else:
-            full_configs.append(full_config)
-            full_configs_ids.append(model_id)
 
         new_id = bmm.get_unique_id(config)
         if new_id != model_id:
-            print(f'Model {shorten(model_id)} has wrong ID!')
+            print(f'Model {model_id} has wrong ID!')
             sane = False
             resp = input('Would you like to rename it? y/n')
             if resp == 'y':
                 new_dir = os.path.join(models_dir, new_id)
                 os.rename(model_dir, new_dir)
-                print(f'Renamed model {shorten(model_id)} to {new_id}')
+                print(f'Renamed model {model_id} to {new_id}')
             else:
                 print('Model was not renamed')
 
