2b3f8e38b3138b6ce524328eae08d89a5d7edc6a
philgzl <services@philgzl.com>
2021-06-30 17:31:29 +0200

    Add ICRA complexity measurement scripts

---
+++ scripts/analyze_icra_dsets.py
@@ -0,0 +1,94 @@
+import argparse
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import brever.pytorchtools as bptt
+import brever.modelmanagement as bmm
+from brever.config import defaults
+from brever.utils import pca
+
+
+def main(args):
+    paths = [
+        'data/processed/test/icra_01',
+        'data/processed/test/icra_02',
+        'data/processed/test/icra_03',
+        'data/processed/test/icra_04',
+        'data/processed/test/icra_05',
+        'data/processed/test/icra_06',
+        'data/processed/test/icra_07',
+        'data/processed/test/icra_08',
+        'data/processed/test/icra_09',
+    ]
+
+    scores_pca = []
+    scores_class = []
+
+    fig_dist, ax_dist = plt.subplots()
+
+    for path in paths:
+
+        # load config file
+        config = defaults()
+        config_file = os.path.join(path, 'config.yaml')
+        config.update(bmm.read_yaml(config_file))
+
+        dset = bptt.H5Dataset(
+            dirpath=path,
+            features=config.POST.FEATURES,
+            labels=config.POST.LABELS,
+            load=config.POST.LOAD,
+            stack=config.POST.STACK,
+            decimation=1,  # there must not be decimation during testing
+            dct_toggle=config.POST.DCT.ON,
+            n_dct=config.POST.DCT.NCOEFF,
+            prestack=config.POST.PRESTACK,
+        )
+
+        features, labels = dset[:]
+
+        components, ve, means = pca(features, fve=0.95)
+        scores_pca.append(components.shape[1])
+
+        scores_class.append((labels < 0.5).mean())
+
+        ax_dist.hist(
+            labels.flatten(),
+            bins=100,
+            label=os.path.basename(path),
+            alpha=0.33,
+        )
+
+    xticks = np.arange(len(paths))
+    xticklabels = [os.path.basename(path) for path in paths]
+    fig, ax = plt.subplots()
+    l1 = ax.bar(
+        xticks-0.33/2,
+        scores_pca,
+        width=0.33,
+        color='tab:blue',
+    )
+    ax.set_ylabel('number of components')
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(xticklabels, rotation=45, ha='right')
+    ax = ax.twinx()
+    l2 = ax.bar(
+        xticks+0.33/2,
+        scores_class,
+        width=0.33,
+        color='tab:orange',
+    )
+    ax.set_ylabel('fraction of zeros')
+    ax.legend([l1, l2], ['pca', 'labels'])
+
+    ax_dist.legend()
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    main(args)
---
+++ scripts/initialize_icra_dsets.py
@@ -0,0 +1,152 @@
+import os
+import itertools
+
+import brever.modelmanagement as bmm
+from brever.config import defaults
+
+
+def main(args, params):
+
+    def_cfg = defaults()
+    processed_dir = def_cfg.PATH.PROCESSED
+
+    new_configs = []
+    new_paths = []
+    old_configs = []
+    old_paths = []
+
+    for room, snr, noise, angle, rms, speech_dset in itertools.product(
+                args.test_rooms,
+                args.test_snrs,
+                args.test_noises,
+                args.test_angles,
+                args.test_rms,
+                args.test_speech_datasets,
+            ):
+        config = {
+            'PRE': {
+                'MIX': {
+                    'NUMBER': args.n_test,
+                    'FILELIMITS': {
+                        'NOISE': [0.85, 1.0],
+                        'TARGET': [0.85, 1.0]
+                    },
+                    'SAVE': True,
+                    'RANDOM': {
+                        'ROOMS': room,
+                        'TARGET': {
+                            'ANGLE': {
+                                'MIN': angle[0],
+                                'MAX': angle[1]
+                            },
+                            'SNR': {
+                                'DISTARGS': [snr[0], snr[1]],
+                                'DISTNAME': 'uniform'
+                            },
+                            'DATASETS': speech_dset
+                        },
+                        'SOURCES': {
+                                'TYPES': noise
+                        },
+                        'RMSDB': {
+                            'ON': rms
+                        }
+                    }
+                }
+            }
+        }
+
+        for param, value in params.items():
+            if value is not None:
+                key_list = bmm.DatasetInitArgParser.arg_to_keys_map[param]
+                bmm.set_dict_field(config, key_list, value)
+
+        def_cfg.update(config)  # throws an error if config is not valid
+
+        dset_id = ''.join(sorted(noise))
+        test_dir = os.path.join(processed_dir, 'test')
+        dset_path = os.path.join(test_dir, dset_id)
+
+        if not os.path.exists(dset_path):
+            new_configs.append(config)
+            new_paths.append(dset_path)
+        else:
+            old_configs.append(config)
+            old_paths.append(dset_path)
+
+    print(f'{len(new_paths) + len(old_paths)} datasets attempted to be '
+          'initialized.')
+    print(f'{len(old_paths)} already exist.')
+
+    if not new_paths:
+        print(f'{len(new_paths)} will be initialized.')
+    else:
+        resp = input(f'{len(new_paths)} will be initialized. Continue? y/n')
+        if resp == 'y':
+            for config, dirpath in zip(new_configs, new_paths):
+                if not os.path.exists(dirpath):
+                    os.makedirs(dirpath)
+                config_filepath = os.path.join(dirpath, 'config.yaml')
+                if os.path.exists(config_filepath) and not args.force:
+                    print(f'{config_filepath} already exists')
+                    continue
+                bmm.dump_yaml(config, config_filepath)
+                print(f'Initialized {config_filepath}')
+        else:
+            print('No dataset was initialized.')
+
+
+if __name__ == '__main__':
+    parser = bmm.DatasetInitArgParser(description='initialize test datasets')
+    parser.add_argument('-f', '--force', action='store_true',
+                        help='overwrite config file if already exists')
+    parser.add_argument('--n-test', type=int, default=100,
+                        help='number of test mixtures, defaults to 10')
+    parser.add_argument('--test-rooms', nargs='+',
+                        type=bmm.arg_set_type,
+                        default=[
+                            {'surrey_room_a'},
+                        ],
+                        help='rooms for the grid of test conditions')
+    parser.add_argument('--test-snrs', nargs='+',
+                        type=lambda x: bmm.arg_list_type(x, int),
+                        default=[
+                            [0, 0],
+                        ],
+                        help='snr limits for the grid of test conditions')
+    parser.add_argument('--test-noises', nargs='+',
+                        type=bmm.arg_set_type,
+                        default=[
+                            {'icra_01'},
+                            {'icra_02'},
+                            {'icra_03'},
+                            {'icra_04'},
+                            {'icra_05'},
+                            {'icra_06'},
+                            {'icra_07'},
+                            {'icra_08'},
+                            {'icra_09'},
+                        ],
+                        help='noises for the grid of test conditions')
+    parser.add_argument('--test-angles', nargs='+',
+                        type=lambda x: bmm.arg_list_type(x, int),
+                        default=[
+                            [0.0, 0.0],
+                        ],
+                        help='angle limits for the grid of test conditions')
+    parser.add_argument('--test-rms',  nargs='+',
+                        type=lambda x: bool(int(x)),
+                        default=[
+                            False,
+                        ],
+                        help='random rms for the grid of test conditions')
+    parser.add_argument('--test-speech-datasets',
+                        type=bmm.arg_set_type,
+                        default=[
+                            {'ieee'},
+                        ],
+                        help='speech datasets for the grid of test conditions')
+    dataset_args, args = parser.parse_args()
+    params = vars(dataset_args)
+
+    main(args, params)
