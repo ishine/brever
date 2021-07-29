2cee1c4a9c1e9eb938ee349eb2578160b9868d7b
philgzl <services@philgzl.com>
2021-07-29 10:55:21 +0200

    Update ICRA complexity analysis script

--- scripts/analyze_icra_dsets.py
+++ scripts/analyze_icra_dsets.py
@@ -13,6 +13,11 @@ import ecol
 import ecol.pls
 
 
+def inv_sigmoid(x):
+    eps = np.finfo(float).eps
+    return np.log((x + eps)/(1 - x + eps))
+
+
 def main(args):
     paths = [
         'data/processed/test/icra_01',
@@ -55,34 +60,36 @@ def main(args):
         features, labels = dset[:]
 
         ax_dist.hist(
-            labels.flatten(),
+            inv_sigmoid(labels.flatten()),
             bins=100,
             label=os.path.basename(path),
             alpha=0.33,
         )
 
-        labels = labels > 0.5
+        labels_cont = labels
+        labels_bin = labels > 0.5
 
         n = len(features)
         m = n//10
         i_start = 0
         features = features[i_start:i_start+m]
-        labels = labels[i_start:i_start+m]
+        labels_cont = labels_cont[i_start:i_start+m]
+        labels_bin = labels_bin[i_start:i_start+m]
 
         print('dist mat...')
         dist_mat = pdist(features)
         dist_mat = squareform(dist_mat)
 
         print('N1...')
-        n1 = ecol.N1(features, labels, dist_mat=dist_mat)
+        n1 = ecol.N1(features, labels_bin, dist_mat=dist_mat)
         print('N2...')
-        n2 = ecol.N2(features, labels, dist_mat=dist_mat)
+        n2 = ecol.N2(features, labels_bin, dist_mat=dist_mat)
         print('N3...')
-        n3 = ecol.N3(features, labels, dist_mat=dist_mat)
+        n3 = ecol.N3(features, labels_bin, dist_mat=dist_mat)
         # print('N4...')
-        # n4 = ecol.N4(features, labels)
+        # n4 = ecol.N4(features, labels_bin)
         # print('T1...')
-        # t1 = ecol.T1(features, labels, dist_mat=dist_mat)
+        # t1 = ecol.T1(features, labels_bin, dist_mat=dist_mat)
         print('T2...')
         t2 = ecol.T2(features)
         print('T3...')
@@ -90,21 +97,21 @@ def main(args):
         print('T4...')
         t4 = ecol.T4(features)
         print('CB...')
-        cb = 1 - labels.mean()
+        cb = 1 - labels_bin.mean()
 
         print('PLS...')
         try:
-            pvex, pvey = ecol.pls.pls(features, labels, features.shape[1])
+            pvex, pvey = ecol.pls.pls(features, labels_cont, features.shape[1])
         except np.linalg.LinAlgError:
-            pls1 = 0
-            pls2 = 0
+            pls_x = 0
+            pls_y = 0
         else:
             n_components = np.argmax(np.cumsum(pvex) >= 0.95) + 1
-            pls1 = n_components/features.shape[1]
-            pls2 = pvey.sum()
+            pls_x = n_components/features.shape[1]
+            pls_y = pvey.sum()
 
         scores.append((n1, n2, n3, t2, t3, t4, cb))
-        scores_pls.append((pls1, pls2))
+        scores_pls.append((t4, pls_x, pls_y))
 
     scores = np.asarray(scores)
     xticks = np.arange(len(paths))
@@ -127,7 +134,7 @@ def main(args):
     fig, ax = plt.subplots()
     n, m = scores_pls.shape
     width = 1/(m+1)
-    labels = ['pls1', 'pls2']
+    labels = ['pca', 'pls_x', 'pls_y']
     for j in range(m):
         offset = (j - (m-1)/2)*width
         x = np.arange(n) + offset
