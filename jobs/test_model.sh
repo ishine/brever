bd36a662b1b6eada8529ecdcc99c2dfc3f18f838
philgzl <services@philgzl.com>
2021-08-05 12:28:20 +0200

    Stop checking if scores file exists in job script

--- jobs/test_model.sh
+++ jobs/test_model.sh
@@ -3,7 +3,8 @@ source jobs/parse_args.sh
 
 for input in "$@"
 do
-    if [ -f $input/scores.json ] && [ "$FORCE" == "" ]
+    # if [ -f $input/scores.json ] && [ "$FORCE" == "" ]
+    if false
     then
         echo "model already tested: $input "
     else
