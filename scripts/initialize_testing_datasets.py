9af8c5cb27dd836b47bfae1374682a84dcd29f69
philgzl <services@philgzl.com>
2021-08-09 17:24:55 +0200

    Reduce test duration

--- scripts/initialize_testing_datasets.py
+++ scripts/initialize_testing_datasets.py
@@ -111,8 +111,8 @@ if __name__ == '__main__':
     parser = bmm.DatasetInitArgParser(description='initialize test datasets')
     parser.add_argument('-f', '--force', action='store_true',
                         help='overwrite config file if already exists')
-    parser.add_argument('--test-duration', type=int, default=3600,
-                        help='total duration, defaults to 3600 seconds')
+    parser.add_argument('--test-duration', type=int, default=300,
+                        help='total duration, defaults to 300 seconds')
     parser.add_argument('--test-rooms', nargs='+',
                         type=bmm.arg_set_type,
                         default=[
