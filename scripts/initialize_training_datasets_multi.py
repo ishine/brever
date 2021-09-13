8876561dd1ba139afc23beed60c19ec7910b0c23
philgzl <services@philgzl.com>
2021-09-13 10:47:17 +0200

    Change default conditions

--- scripts/initialize_training_datasets_multi.py
+++ scripts/initialize_training_datasets_multi.py
@@ -11,8 +11,6 @@ def main(args, params):
 
     # grid of dataset parameters
     noise_typess = [
-        # {'bbl'},
-        # {'ssn'},
         {'dcase_airport'},
         {'dcase_bus'},
         {'dcase_metro'},
@@ -32,7 +30,6 @@ def main(args, params):
         {'icra_07'},
         {'icra_08'},
         {'icra_09'},
-        # {'ssn', 'bbl'},
         {'dcase_.*'},
         {'icra_.*'},
         {'demand'},
@@ -43,34 +40,7 @@ def main(args, params):
         {'dcase_.*', 'icra_.*', 'noisex', 'arte'},
         {'dcase_.*', 'icra_.*', 'demand', 'arte'},
         {'dcase_.*', 'icra_.*', 'demand', 'noisex'},
-        {
-            'dcase_airport',
-            'dcase_bus',
-            'dcase_metro',
-            'dcase_metro_station',
-            'dcase_park',
-        },
-        {
-            'dcase_public_square',
-            'dcase_shopping_mall',
-            'dcase_street_pedestrian',
-            'dcase_street_traffic',
-            'dcase_tram',
-        },
-        {
-            'dcase_airport',
-            'dcase_metro',
-            'dcase_park',
-            'dcase_shopping_mall',
-            'dcase_street_traffic',
-        },
-        {
-            'dcase_bus',
-            'dcase_metro_station',
-            'dcase_public_square',
-            'dcase_street_pedestrian',
-            'dcase_tram',
-        },
+        {'dcase_.*', 'icra_.*', 'demand', 'noisex', 'arte'},
     ]
     roomss = [
         {'surrey_anechoic'},
@@ -87,6 +57,7 @@ def main(args, params):
         {'ash_r1[0-9]'},  # 10 to 19
         {'ash_r2[0-9]'},  # 20 to 29
         {'ash_r3[0-9]'},  # 30 to 39
+        {'ash_r[0-1][0248]a?b?'},  # even from 0 to 19
         {'surrey_.*'},
         {'ash_.*'},
         {'air_.*'},
@@ -97,6 +68,7 @@ def main(args, params):
         {'surrey_.*', 'ash_.*', 'catt_.*', 'avil_.*'},
         {'surrey_.*', 'ash_.*', 'air_.*', 'avil_.*'},
         {'surrey_.*', 'ash_.*', 'air_.*', 'catt_.*'},
+        {'surrey_.*', 'ash_.*', 'air_.*', 'catt_.*', 'avil_.*'},
     ]
     angle_limss = [
         [0.0, 0.0],
@@ -128,6 +100,24 @@ def main(args, params):
         {'timit_(f[0-4]?[0-9]|m[0-4]?[0-9])'},  # males and females 0 to 49
         {'timit_(f[4-9][0-9]|m[4-9][0-9])'},  # males and females 49 to 99
         {'timit_(f1[0-4][0-9]|m1[0-4][0-9])'},  # males and females 100 to 149
+        {'timit_(f[0-9]?[02468]|m[0-9]?[02468])'},  # even males and females 0 to 99
+        {'timit_(f[0-9]?[13579]|m[0-9]?[13579])'},  # odd males and females 0 to 99
+        # {'timit_(?!m0$).*'},  # male 0
+        # {'timit_(?!f0$).*'},  # female 0
+        # {'timit_(?!m1$).*'},  # male 1
+        # {'timit_(?!f1$).*'},  # female 1
+        # {'timit_(?!m2$).*'},  # male 2
+        # {'timit_(?!f2$).*'},  # female 2
+        # {'timit_(?!(f[0-4]|m[0-4])$).*'},  # males and females 0 to 4
+        # {'timit_(?!(f[5-9]|m[5-9])$).*'},  # males and females 5 to 9
+        # {'timit_(?!(f1[0-4]|m1[0-4])$).*'},  # males and females 10 to 14
+        # {'timit_(?!(f1[5-9]|m1[5-9])$).*'},  # males and females 15 to 19
+        # {'timit_(?!(f2[0-4]|m2[0-4])$).*'},  # males and females 20 to 24
+        # {'timit_(?!(f[0-4]?[0-9]|m[0-4]?[0-9])$),*'},  # males and females 0 to 49
+        # {'timit_(?!(f[4-9][0-9]|m[4-9][0-9])$),*'},  # males and females 49 to 99
+        # {'timit_(?!(f1[0-4][0-9]|m1[0-4][0-9])$),*'},  # males and females 100 to 149
+        # {'timit_(?!(f[0-9]?[02468]|m[0-9]?[02468])$),*'},  # even males and females 0 to 99
+        # {'timit_(?!(f[0-9]?[13579]|m[0-9]?[13579])$),*'},  # odd males and females 0 to 99
         {'libri_m0'},  # male 0
         {'libri_f0'},  # female 0
         {'libri_m1'},  # male 1
@@ -141,6 +131,23 @@ def main(args, params):
         {'libri_(f2[0-4]|m2[0-4])'},  # males and females 20 to 24
         {'libri_(f[0-4]?[0-9]|m[0-4]?[0-9])'},  # males and females 0 to 49
         {'libri_(f[4-9][0-9]|m[4-9][0-9])'},  # males and females 49 to 99
+        {'libri_(f[0-9]?[02468]|m[0-9]?[02468])'},  # even males and females 0 to 99
+        {'libri_(f[0-9]?[13579]|m[0-9]?[13579])'},  # odd males and females 0 to 99
+        # {'libri_(?!m0$).*'},  # male 0
+        # {'libri_(?!f0$).*'},  # female 0
+        # {'libri_(?!m1$).*'},  # male 1
+        # {'libri_(?!f1$).*'},  # female 1
+        # {'libri_(?!m2$).*'},  # male 2
+        # {'libri_(?!f2$).*'},  # female 2
+        # {'libri_(?!(f[0-4]|m[0-4])$).*'},  # males and females 0 to 4
+        # {'libri_(?!(f[5-9]|m[5-9])$).*'},  # males and females 5 to 9
+        # {'libri_(?!(f1[0-4]|m1[0-4])$).*'},  # males and females 10 to 14
+        # {'libri_(?!(f1[5-9]|m1[5-9])$).*'},  # males and females 15 to 19
+        # {'libri_(?!(f2[0-4]|m2[0-4])$).*'},  # males and females 20 to 24
+        # {'libri_(?!(f[0-4]?[0-9]|m[0-4]?[0-9])$).*'},  # males and females 0 to 49
+        # {'libri_(?!(f[4-9][0-9]|m[4-9][0-9])$).*'},  # males and females 49 to 99
+        # {'libri_(?!(f[0-9]?[02468]|m[0-9]?[02468])$).*'},  # even males and females 0 to 99
+        # {'libri_(?!(f[0-9]?[13579]|m[0-9]?[13579])$).*'},  # odd males and females 0 to 99
         {'ieee'},
         {'timit_.*'},
         {'libri_.*'},
@@ -151,17 +158,18 @@ def main(args, params):
         {'ieee', 'libri_.*', 'arctic', 'hint'},
         {'ieee', 'libri_.*', 'timit_.*', 'hint'},
         {'ieee', 'libri_.*', 'timit_.*', 'arctic'},
+        {'ieee', 'libri_.*', 'timit_.*', 'arctic', 'hint'},
     ]
 
     # the default config is a very specialized one
     def add_config(
                 configs,
-                noise_types={'dcase_airport'},
-                rooms={'surrey_room_a'},
+                noise_types={'dcase_.*'},
+                rooms={'surrey_.*'},
                 angle_lims=[0.0, 0.0],
-                snr_lims=[0, 0],
+                snr_lims=[-5, 10],
                 rms_jitter=False,
-                speakers={'ieee'},
+                speakers={'timit_.*'},
             ):
 
         for basename, filelims, duration, seed in zip(
