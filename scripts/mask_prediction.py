d364de07befbdc11931bfe35f235ac542ce2653c
philgzl <services@philgzl.com>
2021-10-12 11:54:15 +0200

    Update mask prediction script

--- scripts/mask_prediction.py
+++ scripts/mask_prediction.py
@@ -2,17 +2,15 @@ import argparse
 import os
 import random
 import pickle
-import re
 
 import torch
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
-import scipy.io
 
 from brever.config import defaults
 import brever.pytorchtools as bptt
-from brever.display import plot_spectrogram, share_clim
+import brever.display as bplot
 import brever.modelmanagement as bmm
 
 
@@ -26,44 +24,31 @@ def main(args):
     n_cols = 2
     n_models = len(args.input)
     fig_run, axes_run = plt.subplots(n_rows, n_cols)
+    if axes_run.ndim == 1:
+        axes_run = axes_run.reshape(1, len(axes_run))
 
-    for i_model, model_dirpath in enumerate(args.input):
+    for i_model, model_dir in enumerate(args.input):
         # load model configuration
         config = defaults()
-        config_file = os.path.join(model_dirpath, 'config.yaml')
+        config_file = os.path.join(model_dir, 'config.yaml')
         config.update(bmm.read_yaml(config_file))
 
         # initialize and load model
-        model_args_path = os.path.join(model_dirpath, 'model_args.yaml')
+        model_args_path = os.path.join(model_dir, 'model_args.yaml')
         model = bptt.Feedforward.build(model_args_path)
-        state_file = os.path.join(model_dirpath, 'checkpoint.pt')
+        state_file = os.path.join(model_dir, 'checkpoint.pt')
         model.load_state_dict(torch.load(state_file, map_location='cpu'))
-        if config.MODEL.CUDA and not args.no_cuda:
-            model = model.cuda()
 
         # get dataset path
-        snrs = [0, 3, 6, 9, 12, 15]
-        room_aliases = [
-            'surrey_room_a',
-            'surrey_room_b',
-            'surrey_room_c',
-            'surrey_room_d',
-        ]
         if args.dataset is None:
-            # choose random snr and room
-            snr = random.choice(snrs)
-            room_alias = random.choice(room_aliases)
-            suffix = f'snr{snr}_room{room_alias[-1].upper()}'
-            test_dataset_dir = f'{config.POST.PATH.TEST}_{suffix}'
+            # choose random data in the list of test paths
+            test_dir = random.choice(sorted(config.POST.PATH.TEST))
         else:
-            test_dataset_dir = args.dataset
-            m = re.match('.+snr(.{1,2})_room(.)', test_dataset_dir)
-            snr = int(m.group(1))
-            room_alias = f'surrey_room_{m.group(2).lower()}'
+            test_dir = args.dataset
 
         # load test dataset
         test_dataset = bptt.H5Dataset(
-            dirpath=test_dataset_dir,
+            dirpath=test_dir,
             features=config.POST.FEATURES,
             labels=config.POST.LABELS,
             load=config.POST.LOAD,
@@ -76,11 +61,11 @@ def main(args):
 
         # set normalization
         if config.POST.NORMALIZATION.TYPE == 'global':
-            stat_path = os.path.join(model_dirpath, 'statistics.npy')
+            stat_path = os.path.join(model_dir, 'statistics.npy')
             mean, std = np.load(stat_path)
             test_dataset.transform = bptt.TensorStandardizer(mean, std)
         elif config.POST.NORMALIZATION.TYPE == 'recursive':
-            stat_path = os.path.join(model_dirpath, 'statistics.npy')
+            stat_path = os.path.join(model_dir, 'statistics.npy')
             mean, std = np.load(stat_path)
             test_dataset.transform = bptt.ResursiveTensorStandardizer(
                 mean=mean,
@@ -101,16 +86,16 @@ def main(args):
                              f'{config.POST.NORMALIZATION.TYPE}')
 
         # load pipes
-        pipes_file = os.path.join(config.POST.PATH.TRAIN, 'pipes.pkl')
+        pipes_file = os.path.join(test_dir, 'pipes.pkl')
         with open(pipes_file, 'rb') as f:
             pipes = pickle.load(f)
         scaler = pipes['scaler']
         filterbank = pipes['filterbank']
         framer = pipes['framer']
 
-        # load pesq score
-        pesq_filepath = os.path.join(model_dirpath, 'pesq_scores.mat')
-        pesq = scipy.io.loadmat(pesq_filepath)['scores']
+        # load scores
+        scores_file = os.path.join(model_dir, 'scores.json')
+        scores = bmm.read_json(scores_file)
 
         # open dataset
         with h5py.File(test_dataset.filepath, 'r') as f:
@@ -118,19 +103,22 @@ def main(args):
             # select mixture
             if args.mix_index is None:
                 # select random mixture
-                n = len(f['mixtures'])
+                n = len(f['mixture'])
                 k = random.randrange(n)
             else:
                 k = args.mix_index
-            print(f'Predicting mixture number {k} in {test_dataset_dir}')
+            print(f'Predicting mixture number {k} in {test_dir}')
 
             # grab pesq
-            pesq = pesq[snrs.index(snr), room_aliases.index(room_alias), k]
+            pesq_in = scores[test_dir]['ref']['PESQ'][k]
+            pesq_out = scores[test_dir]['model']['PESQ'][k]
+            dpesq = pesq_out - pesq_in
 
             # load mixture
-            mixture = f['mixtures'][k].reshape(-1, 2)
-            foreground = f['foregrounds'][k].reshape(-1, 2)
-            background = f['backgrounds'][k].reshape(-1, 2)
+            mixture = f['mixture'][k].reshape(-1, 2)
+            foreground = f['foreground'][k].reshape(-1, 2)
+            # background = f['background'][k].reshape(-1, 2)
+            background = foreground
 
         # scale signal
         scaler.fit(mixture)
@@ -147,15 +135,11 @@ def main(args):
         i_start, i_end = test_dataset.file_indices[k]
         features, labels = test_dataset[i_start:i_end]
         features = torch.from_numpy(features).float()
-        if config.MODEL.CUDA and not args.no_cuda:
-            features = features.cuda()
 
         # make RM prediction
         model.eval()
         with torch.no_grad():
             PRM = model(features)
-            if config.MODEL.CUDA and not args.no_cuda:
-                PRM = PRM.cpu()
             PRM = PRM.numpy()
         mse = np.mean((PRM-labels)**2)
 
@@ -204,12 +188,12 @@ def main(args):
                 set_kw = {'title': var, 'ylabel': ''}
             elif var == 'PRM':
                 f = filterbank.fc
-                title = f'PRM, dPESQ: {pesq:.2f}, MSE: {mse*1e3:.2f}e-3'
+                title = f'dPESQ: {dpesq:.2f}, MSE: {mse*1e3:.2f}e-3'
                 set_kw = {'title': title}
             else:
                 f = filterbank.fc
                 set_kw = {'title': var}
-            plot_spectrogram(
+            bplot.plot_spectrogram(
                 locals()[var],
                 ax=axes[i],
                 fs=config.PRE.FS,
@@ -219,7 +203,7 @@ def main(args):
             )
 
         # plot on running fig
-        plot_spectrogram(
+        bplot.plot_spectrogram(
             labels,
             ax=axes_run[i_model, 0],
             fs=config.PRE.FS,
@@ -227,10 +211,9 @@ def main(args):
             f=filterbank.fc,
             set_kw={'title': 'labels'},
         )
-        model_id = os.path.basename(os.path.dirname(model_dirpath))
-        title = f'{model_id[:6]}..., dPESQ: {pesq:.2f}, MSE: {mse*1e3:.2f}e-3'
+        title = f'{model_dir}, dPESQ: {dpesq:.2f}, MSE: {mse*1e3:.2f}e-3'
         set_kw = {'title': title}
-        plot_spectrogram(
+        bplot.plot_spectrogram(
             PRM,
             ax=axes_run[i_model, 1],
             fs=config.PRE.FS,
@@ -242,24 +225,13 @@ def main(args):
         # match signal spectrograms clims
         vars__ = ['mixture', 'foreground', 'background']
         axes_ = [axes[vars_.index(var)] for var in vars__]
-        share_clim(axes_)
+        bplot.share_clim(axes_)
 
         # set IRM and PRM clims to 0 and 1
         vars__ = ['labels', 'PRM']
         axes_ = [axes[vars_.index(var)] for var in vars__]
         for ax in axes_:
             ax.images[0].set_clim(0, 1)
-
-    # # plot feature and label distribution
-    # fig, axes = plt.subplots(2, 1)
-    # for ax, data, title in zip(
-    #             axes,
-    #             [features_raw, labels],
-    #             ['Features (not normalized)', 'Labels'],
-    #         ):
-    #     ax.hist(data.flatten(), bins=50)
-    #     ax.set_title(title)
-    # fig.tight_layout()
 
     # show
     plt.show()
@@ -268,10 +240,8 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='make random mask prediction')
     parser.add_argument('input', nargs='+', help='input model directories')
-    parser.add_argument('--seed', type=int, help='seed')
+    parser.add_argument('--seed', type=int, help='seed', default=0)
     parser.add_argument('--dataset', help='dataset directory path')
     parser.add_argument('--mix-index', type=int, help='mixture index')
-    parser.add_argument('--no-cuda', action='store_true',
-                        help='force model loading on cpu')
     args = parser.parse_args()
     main(args)
