3e395d03703fecdc5e932fa5b2a3dce585645468
philgzl <services@philgzl.com>
2022-02-07 15:55:41 +0100

    Use 32GB GPU

--- jobs/job.sh
+++ jobs/job.sh
@@ -5,13 +5,14 @@
 ### -- set the job Name --
 #BSUB -J jobname
 ### -- ask for number of cores (default: 1) --
-#BSUB -n 1
+#BSUB -n 4
 ### -- Select the resources: 1 gpu in exclusive process mode --
 #BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -R "select[gpu32gb]"
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
 #BSUB -W 24:00
-# request 16GB of system-memory per core
-#BSUB -R "rusage[mem=16GB]"
+# request 4GB of system-memory per core
+#BSUB -R "rusage[mem=4GB]"
 ### -- Specify the output and error file. %J is the job-id --
 ### -- -o and -e mean append, -oo and -eo mean overwrite --
 #BSUB -oo jobs/logs/%J.out
