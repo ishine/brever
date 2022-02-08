69540044254b7bbb8e7d0377aeb3770c28755137
philgzl <services@philgzl.com>
2022-02-08 11:13:41 +0100

    Update job

--- jobs/job_hpc.sh
+++ jobs/job_hpc.sh
@@ -5,11 +5,11 @@
 ### -- set the job Name --
 #BSUB -J jobname
 ### -- ask for number of cores (default: 1) --
-#BSUB -n 8
+#BSUB -n 4
 ### -- set walltime limit: hh:mm
-#BSUB -W 16:00
-# request 1GB of system-memory
-#BSUB -R "rusage[mem=1GB]"
+#BSUB -W 24:00
+# request 4GB of system-memory per core
+#BSUB -R "rusage[mem=4GB]"
 ### -- Specify the output and error file. %J is the job-id --
 ### -- -o and -e mean append, -oo and -eo mean overwrite --
 #BSUB -oo jobs/logs/%J.out
