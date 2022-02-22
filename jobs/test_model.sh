d74f768cb0628fcf625a10809039d52c9cc5be8a
philgzl <services@philgzl.com>
2022-02-22 16:16:45 +0100

    Update evaluation job script

--- jobs/test_model.sh
+++ jobs/test_model.sh
@@ -1,13 +1,83 @@
 #!/bin/sh
-source jobs/parse_args.sh
 
-for input in "$@"
-do
-    # if [ -f $input/scores.json ] && [ "$FORCE" == "" ]
-    if false
-    then
-        echo "model already tested: $input "
-    else
-        bash jobs/submit.sh jobs/job_hpc.sh "python scripts/test_model.py $input$FORCE --no-cuda"
-    fi
+OPTS=$(getopt \
+    --options fvq:c:m:w:g \
+    --longoptions force,verbose,queue:,cores:,memory:,walltime:,gpu32gb \
+    -- "$@"
+)
+if [ $? -ne 0 ]; then exit 1; fi
+eval set -- "$OPTS"
+
+FORCE=false
+VERBOSE=false
+QUEUE="hpc"
+CORES=1
+MEMORY=8
+WALLTIME=24
+GPU32GB=false
+
+while true; do
+  case "$1" in
+    -f | --force ) FORCE=true; shift ;;
+    -v | --verbose ) VERBOSE=true; shift ;;
+    -q | --queue ) QUEUE="$2"; shift; shift ;;
+    -c | --cores ) CORES="$2"; shift; shift ;;
+    -m | --memory ) MEMORY="$2"; shift; shift ;;
+    -w | --walltime ) WALLTIME="$2"; shift; shift ;;
+    -g | --gpu32gb ) GPU32GB=true; shift ;;
+    -- ) shift; break ;;
+  esac
 done
+
+if [ ${QUEUE} != "hpc" ] && [ ${QUEUE} != "gpuv100" ] && [ ${QUEUE} != "gpua100" ]
+then
+    echo "requested wrong queue: ${QUEUE}"
+    exit 1
+fi
+
+JOBFILE="jobs/job.sh"
+echo "#!/bin/sh" > ${JOBFILE}
+echo "#BSUB -q ${QUEUE}" >> ${JOBFILE}
+echo "#BSUB -J jobname" >> ${JOBFILE}
+echo "#BSUB -n ${CORES}" >> ${JOBFILE}
+echo "#BSUB -W ${WALLTIME}:00" >> ${JOBFILE}
+echo "#BSUB -R \"rusage[mem=${MEMORY}GB]\"" >> ${JOBFILE}
+echo "#BSUB -R \"span[hosts=1]\"" >> ${JOBFILE}
+echo "#BSUB -oo jobs/logs/%J.out" >> ${JOBFILE}
+echo "#BSUB -eo jobs/logs/%J.err" >> ${JOBFILE}
+if [ ${QUEUE} == "gpuv100" ] || [ ${QUEUE} == "gpua100" ]
+then
+    echo "#BSUB -gpu \"num=1:mode=exclusive_process\"" >> ${JOBFILE}
+    if [ ${GPU32GB} = true ]
+    then
+        echo "#BSUB -R \"select[gpu32gb]\"" >> ${JOBFILE}
+    fi
+fi
+
+echo "source venv/bin/activate" >> ${JOBFILE}
+echo "COMMAND" >> ${JOBFILE}
+
+if [ ${VERBOSE} = true ]
+then
+    echo "The following job template was created:"
+    echo "---Beginning of file---"
+    cat ${JOBFILE}
+    echo "---End of file---"
+fi
+
+if [ $# != 2 ]
+then
+    echo "ERROR: 2 positional arguments are required"
+    exit 1
+fi
+
+COMMAND="python scripts/test_model.py $1 $2"
+if [ ${FORCE} = true ]
+then
+    COMMAND="${COMMAND} -f"
+fi
+if [ ${VERBOSE} = true ]
+then
+    echo "Submitting \"${COMMAND}\" as COMMAND"
+fi
+bash jobs/submit.sh jobs/job.sh "${COMMAND}"
