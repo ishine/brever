627a034ee0ad138fe0e3820d1ba1e26bde0c24ca
philgzl <services@philgzl.com>
2022-04-12 15:46:32 +0200

    Update corpus details script

--- scripts/corpus_details.py
+++ scripts/corpus_details.py
@@ -29,23 +29,28 @@ def main():
     loader = AudioFileLoader()
 
     dict_ = {}
-    for prefix in [
+    for db in [
         "timit",
         "libri",
         "clarity",
         "wsj0",
         "vctk",
     ]:
-        spks = loader.get_speakers(prefix)
-        dict_[prefix] = {}
-        dict_[prefix]['speakers'] = len(spks)
+        spks = loader.get_speakers(db)
+        dict_[db] = {}
+        dict_[db]['speakers'] = len(spks)
         utts = sum(len(x) for x in spks.values())
-        dict_[prefix]['utterances'] = utts
-        dict_[prefix]['min_utt/spk'] = min(len(x) for x in spks.values())
-        dict_[prefix]['max_utt/spk'] = max(len(x) for x in spks.values())
-        duration = loader.get_duration(f'{prefix}_.*')[0]
-        dict_[prefix]['total_length'] = f"{duration/3600:.1f}h"
-        dict_[prefix]['avg_utt_length'] = f"{duration/utts:.1f}s"
+        utts_per_spk = [len(x) for x in spks.values()]
+        dict_[db]['utterances'] = utts
+        dict_[db]['avg_utt/spk'] = round(sum(utts_per_spk)/len(utts_per_spk))
+        dict_[db]['min_utt/spk'] = min(utts_per_spk)
+        dict_[db]['max_utt/spk'] = max(utts_per_spk)
+        duration = loader.get_duration(f'{db}_.*', reduce_=False)[0]
+        total = sum(duration)
+        dict_[db]['duration'] = f"{total/3600:.1f}h"
+        dict_[db]['avg_utt_len'] = f"{total/utts:.1f}s"
+        dict_[db]['min_utt_len'] = f"{min(duration):.1f}s"
+        dict_[db]['max_utt_len'] = f"{max(duration):.1f}s"
 
     pretty_table(dict_, "corpus")
 
