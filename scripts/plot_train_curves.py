58ae9312a58f3839e5404dbaff6925ff428464ec
philgzl <services@philgzl.com>
2022-04-21 17:31:18 +0200

    Add legend optional argument in plot train curve script

--- scripts/plot_train_curves.py
+++ scripts/plot_train_curves.py
@@ -5,6 +5,9 @@ from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
 import numpy as np
 
+from brever.args import ModelArgParser
+from brever.config import get_config
+
 
 def main():
     plt.rc('axes', facecolor='#E6E6E6', edgecolor='none', axisbelow=True)
@@ -14,7 +17,20 @@ def main():
     for model in args.inputs:
         path = os.path.join(model, 'losses.npz')
         data = np.load(path)
-        l, = ax.plot(data['train'], label=model)
+
+        if args.legend_params is None:
+            label = model
+        else:
+            config = get_config(os.path.join(model, 'config.yaml'))
+            label = {}
+            for param_name in args.legend_params:
+                label[param_name] = config.get_field(
+                    ModelArgParser.arg_map[config.ARCH][param_name]
+                )
+            label = [f'{key}: {val}' for key, val in label.items()]
+            label = ', '.join(label)
+
+        l, = ax.plot(data['train'], label=label)
         _, = ax.plot(data['val'], '--', color=l.get_color())
 
     lines = [
@@ -37,6 +53,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='plot training curves')
     parser.add_argument('inputs', nargs='+',
                         help='paths to model directories')
+    parser.add_argument('--legend-params', nargs='+',
+                        help='hyperparameters to use to label curves')
     parser.add_argument('--ymin', type=float)
     parser.add_argument('--ymax', type=float)
     args = parser.parse_args()
