d0252f453896bac84a8f944572fb11fc6e811825
philgzl <services@philgzl.com>
2022-06-10 13:49:26 +0200

    Update results scripts

--- scripts/make_cross_corpus_table.py
+++ scripts/make_cross_corpus_table.py
@@ -9,7 +9,7 @@ from brever.args import arg_type_path
 from brever.config import DatasetInitializer, ModelInitializer
 
 plt.rcParams['svg.fonttype'] = 'none'
-plt.rcParams['font.size'] = 4
+plt.rcParams['font.size'] = 6
 plt.rcParams['patch.linewidth'] = .5
 plt.rcParams['hatch.linewidth'] = .5
 plt.rcParams['lines.linewidth'] = .5
@@ -20,9 +20,12 @@ plt.rcParams['xtick.major.width'] = .5
 plt.rcParams['ytick.major.size'] = 1
 plt.rcParams['ytick.major.width'] = .5
 plt.rcParams['figure.dpi'] = 200
+plt.rcParams['xtick.major.pad'] = 2
+plt.rcParams['ytick.major.pad'] = 2
+plt.rcParams['font.family'] = "Liberation Serif"
+plt.rcParams['mathtext.fontset'] = "stix"
 
-MM_TO_INCH = 0.0393701
-FIGSIZE = (160*MM_TO_INCH, 90*MM_TO_INCH)
+FIGSIZE_HEAT = (6.30*1.15, 3.19*1.15)
 
 RAW_MATH = False
 
@@ -48,6 +51,23 @@ dims = {
         'catt_.*',
         'avil_.*',
     ],
+}
+db_labels = {
+    'timit_.*': 'TIMIT',
+    'libri_.*': 'LibriSpeech',
+    'wsj0_.*': 'WSJ',
+    'clarity_.*': 'Clarity',
+    'vctk_.*': 'VCTK',
+    'dcase_.*': 'TAU',
+    'noisex_.*': 'NOISEX',
+    'icra_.*': 'ICRA',
+    'demand': 'DEMAND',
+    'arte': 'ARTE',
+    'surrey_.*': 'Surrey',
+    'ash_.*': 'ASH',
+    'bras_.*': 'BRAS',
+    'catt_.*': 'CATT',
+    'avil_.*': 'AVIL',
 }
 dim_labels = ['Speech', 'Noise', 'Room']
 archs = ['dnn', 'convtasnet']
@@ -154,15 +174,12 @@ def gather_scores():
 
 
 def plot_heatmaps(scores):
-    title_x = [0.20, 0.525, 0.855]
-    title_y = 0.95
-
-    fig = plt.figure(figsize=FIGSIZE)
+    fig = plt.figure(figsize=FIGSIZE_HEAT)
     outer_gs = gridspec.GridSpec(1, 3, figure=fig)
     for i_dim, (dim, vals) in enumerate(dims.items()):
         inner_gs = gridspec.GridSpecFromSubplotSpec(
             3, len(archs), subplot_spec=outer_gs[i_dim],
-            hspace=0.01, wspace=0.05
+            hspace=0.05, wspace=0.025,
         )
         for i_arch in range(len(archs)):
             for i_ax, i_metric in enumerate(range(3, 6)):
@@ -171,12 +188,17 @@ def plot_heatmaps(scores):
                 vmax = np.percentile(scores[..., i_metric], 95)
                 data = scores[i_arch, i_dim, :, :, i_metric]
                 ax.imshow(data, vmin=vmin, vmax=vmax, cmap='OrRd')
-                ticklabels = [val.replace('_.*', '') for val in vals]
+                ticklabels = [db_labels[val] for val in vals]
                 ax.set_xticks(np.arange(5))
-                ax.set_xticklabels(ticklabels, rotation=20)
+                if i_ax == 2:
+                    ax.set_xticklabels(ticklabels, rotation=35, ha='right',
+                                       rotation_mode='anchor')
+                else:
+                    ax.set_xticklabels([])
                 ax.set_yticks(np.arange(5))
                 if i_arch == 0:
-                    ax.set_yticklabels(ticklabels)
+                    ax.set_yticklabels(ticklabels, rotation=35, ha='right',
+                                       rotation_mode='anchor')
                 else:
                     ax.set_yticklabels([])
                 annotate_heatmap(ax, data, vmin, vmax)
@@ -185,10 +207,7 @@ def plot_heatmaps(scores):
                 if i_dim == 0 and i_arch == 0:
                     ax.set_ylabel(_m(metrics[i_metric]), fontsize='large')
 
-        fig.text(title_x[i_dim], title_y, dim_labels[i_dim],
-                 fontsize='x-large', ha='center')
-
-    fig.tight_layout(rect=(0, 0, 1, 0.95), w_pad=1.6)
+    fig.tight_layout(rect=(0, 0, 1, 0.95), w_pad=0.5)
     fig.patch.set_visible(False)
     fig.savefig('results_heatmaps.svg', bbox_inches=0)
 
@@ -199,104 +218,13 @@ def annotate_heatmap(ax, data, vmin, vmax):
             val = data[i, j]
             loc_on_scale = (val-vmin)/(vmax-vmin)
             color = 'w' if loc_on_scale > 0.4 else 'gray'
-            ax.text(j, i, f'{val:.2f}', ha='center', va='center', color=color)
-
-
-def plot_bars(scores, scores_ref):
-    colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
-    hatch = ['', '////']
-    ylims = [
-        (0, 0.69),
-        (0, 0.15),
-        (0, 9.9),
-    ]
-    yticks = [
-        np.linspace(0, 0.6, 7),
-        np.linspace(0, 0.14, 8),
-        np.linspace(0, 8, 5),
-    ]
-    bar_xspace = 1
-    title_x = [0.21, 0.525, 0.84]
-    title_y = 0.95
-    config_names = [r'$N=1$', r'$N=4$']
-
-    x = np.arange(len(archs)*2).reshape(len(archs), 2)
-    x += bar_xspace*np.arange(len(archs)).reshape(-1, 1)
-    xlim = x.min() - bar_xspace - 0.5, x.max() + bar_xspace + 0.5
-
-    fig = plt.figure(figsize=FIGSIZE)
-    outer_gs = gridspec.GridSpec(1, 3, figure=fig)
-    for i_dim, (dim, vals) in enumerate(dims.items()):
-        inner_gs = gridspec.GridSpecFromSubplotSpec(
-            3, 2, subplot_spec=outer_gs[i_dim], hspace=0.05, wspace=0.05
-        )
-        for i_ax, i_metric in enumerate(range(3, 6)):
-            for i_config in range(2):
-                ax = fig.add_subplot(inner_gs[i_ax, i_config])
-                for i_arch in range(len(archs)):
-
-                    data = scores[i_arch, i_dim, :, :, i_metric]
-                    data_ref = scores_ref[i_arch, i_dim, :, :, i_metric]
-
-                    if i_config == 0:
-                        data = (data.sum(axis=0) - np.diag(data))/4
-                        data_ref = (data_ref.sum(axis=0) - np.diag(data_ref))/4
-                    else:
-                        data, data_ref = np.diag(data_ref), np.diag(data)
-
-                    for is_ref, data_ in enumerate([data, data_ref]):
-                        label = arch_labels[i_arch]
-                        label = label + '-ref' if is_ref else label
-                        ax.bar(x[i_arch, is_ref], data_.mean(),
-                               color=colors[i_arch], hatch=hatch[is_ref],
-                               width=1, label=label, edgecolor='black',
-                               yerr=data_.std())
-
-                    draw_gen_gap(ax, x, i_arch, data, data_ref, ylims[i_ax])
-
-                ax.set_xticks([])
-                ax.set_xlim(xlim)
-                if i_dim == 0 and i_config == 0:
-                    ax.set_ylabel(_m(metrics[i_metric]), fontsize='large')
-                if i_ax == 2:
-                    ax.set_xlabel(_m(config_names[i_config]), fontsize='large')
-                ax.set_axisbelow(True)
-                ax.grid(True, axis='y')
-                ax.set_yticks(yticks[i_ax])
-                ax.set_ylim(ylims[i_ax])
-                if i_dim != 0 or i_config != 0:
-                    ax.set_yticklabels([])
-
-        fig.text(title_x[i_dim], title_y, dim_labels[i_dim],
-                 fontsize='x-large', ha='center')
-
-        handles, labs = ax.get_legend_handles_labels()
-    fig.legend(handles, labs, loc='lower center', ncol=len(archs)*2,
-               fontsize='large')
-    fig.tight_layout(rect=(0, 0.04, 1, 0.95), w_pad=1.8)
-    fig.patch.set_visible(False)
-    fig.savefig('results_bars.svg', bbox_inches=0)
-
-
-def draw_gen_gap(ax, x, i_arch, data, data_ref, ylims):
-    head_length = (ylims[1]-ylims[0])*0.027
-    head_width = 0.22
-    x = x[i_arch, 1] - 1.5
-    y = data_ref.mean()
-    dx = 0
-    dy = data.mean() - data_ref.mean() + head_length
-    dy = min(-1e-3, dy)
-    ax.arrow(x, y, dx, dy, head_length=head_length, head_width=head_width,
-             fc='k', length_includes_head=True, linewidth=.5)
-    G_e = ((data-data_ref)/data_ref).mean()
-    G_e = rf'{round(100*G_e)}%'
-    ax.annotate(G_e, (x+0.3, y+head_length*1.5), ha='center')
+            ax.text(j, i, f'{val:.2f}', ha='center', va='center', color=color,
+                    fontsize=5.5)
 
 
 def main():
     scores, scores_ref = gather_scores()
     plot_heatmaps(scores)
-    plot_bars(scores, scores_ref)
     plt.show()
 
 
