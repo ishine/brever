cee65bc69c558b26848d86fc5e16f2f2d9969693
philgzl <services@philgzl.com>
2022-06-23 13:44:52 +0200

    Update cross validation results script

--- scripts/combined_mismatch_results.py
+++ scripts/combined_mismatch_results.py
@@ -1,8 +1,6 @@
 import os
 import json
-import random
 import itertools
-import copy
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,7 +8,6 @@ import matplotlib.gridspec as gridspec
 
 from brever.args import arg_type_path
 from brever.config import DatasetInitializer, ModelInitializer
-from brever.display import pretty_table
 
 plt.rcParams['svg.fonttype'] = 'none'
 plt.rcParams['font.size'] = 7
@@ -30,29 +27,38 @@ plt.rcParams['mathtext.fontset'] = "stix"
 
 RAW_MATH = False
 
-dim_dict = {
-    'speakers': [
-        'timit_.*',
-        'libri_.*',
-        'wsj0_.*',
-        'clarity_.*',
-        'vctk_.*',
-    ],
-    'noises': [
-        'dcase_.*',
-        'noisex_.*',
-        'icra_.*',
-        'demand',
-        'arte',
-    ],
-    'rooms': [
-        'surrey_.*',
-        'ash_.*',
-        'bras_.*',
-        'catt_.*',
-        'avil_.*',
-    ],
-}
+databases = [
+    {
+        'kwarg': 'speakers',
+        'databases': [
+            'timit_.*',
+            'libri_.*',
+            'wsj0_.*',
+            'clarity_.*',
+            'vctk_.*',
+        ],
+    },
+    {
+        'kwarg': 'noises',
+        'databases': [
+            'dcase_.*',
+            'noisex_.*',
+            'icra_.*',
+            'demand',
+            'arte',
+        ],
+    },
+    {
+        'kwarg': 'rooms',
+        'databases': [
+            'surrey_.*',
+            'ash_.*',
+            'bras_.*',
+            'catt_.*',
+            'avil_.*',
+        ],
+    },
+]
 dim_labels = ['Speech', 'Noise', 'Room']
 archs = ['dnn', 'convtasnet']
 arch_labels = ['FFNN', 'Conv-TasNet']
@@ -107,136 +113,118 @@ def get_test_dset(
 
 
 def get_model(arch, train_path):
-    kwargs = {}
-    if arch == 'convtasnet-k=2':
-        arch = 'convtasnet'
-        kwargs['sources'] = ['foreground', 'background']
     return model_init.get_path_from_kwargs(
         arch=arch,
         train_path=arg_type_path(train_path),
-        **kwargs,
     )
 
 
-def get_scores(model, test_path):
-    if isinstance(test_path, list):
-        out = []
-        for p in test_path:
-            out.append(get_scores(model, p))
-        return np.mean(out, axis=0)
+def complement(idx_list):
+    return [i for i in range(5) if i not in idx_list]
+
+
+def n_eq_one(i, dims):
+    return [[i], [i], [i]]
+
+
+def n_eq_four(i, dims):
+    return [complement([i])]*3
+
+
+def build_test_index(index, dims):
+    test_index = [complement(index[dim]) for dim in range(3)]
+    for dim in dims:
+        test_index[dim] = index[dim]
+    return test_index
+
+
+def build_kwargs(index):
+    kwargs = {}
+    for dim_dbs, dbs_idx in zip(databases, index):
+        kwargs[dim_dbs['kwarg']] = {dim_dbs['databases'][i] for i in dbs_idx}
+    return kwargs
+
+
+def gather_all_scores():
+    # shape = (N, mismatch_scenario, folds, arch, metrics)
+    shape = (2, 8, 5, 2, 6)
+    scores = np.empty(shape)
+    ref_scores = np.empty(shape)
+
+    for i_n, index_func in enumerate([n_eq_one, n_eq_four]):
+
+        i_mismatch = 0
+
+        for ndim in range(3):
+            for dims in itertools.combinations(range(3), ndim):
+                for i_fold in range(5):
+
+                    train_index = index_func(i_fold, dims)
+                    train_kwargs = build_kwargs(train_index)
+                    train_path = get_train_dset(**train_kwargs)
+                    test_idx = build_test_index(train_index, dims)
+                    test_kwargs = build_kwargs(test_idx)
+                    ref_train_path = get_train_dset(**test_kwargs)
+
+                    test_paths = get_test_dsets(test_idx)
+
+                    for i_arch, arch in enumerate(archs):
+                        m = get_model(arch, train_path)
+                        m_ref = get_model(arch, ref_train_path)
+
+                        scores[i_n, i_mismatch, i_fold, i_arch, :] = \
+                            get_scores(m, test_paths)
+                        ref_scores[i_n, i_mismatch, i_fold, i_arch, :] = \
+                            get_scores(m_ref, test_paths)
+
+                i_mismatch += 1
+
+    # last mismatch scenario: matched case
+    for i_n, index_func in enumerate([n_eq_one, n_eq_four]):
+        for dims in [(0, 1, 2)]:
+            for i_fold in range(5):
+
+                index = index_func(i_fold, dims)
+                kwargs = build_kwargs(index)
+                train_path = get_train_dset(**kwargs)
+                test_paths = get_test_dsets(index)
+
+                for i_arch, arch in enumerate(archs):
+                    m = get_model(arch, train_path)
+
+                    score = get_scores(m, test_paths)
+                    scores[i_n, -1, i_fold, i_arch, :] = score
+                    ref_scores[i_n, -1, i_fold, i_arch, :] = score
+
+    return scores, ref_scores
+
+
+def get_scores(model, test_paths):
     score_file = os.path.join(model, 'scores.json')
     with open(score_file) as f:
         scores = json.load(f)
-    pesq = np.mean(scores[test_path]['model']['PESQ'])
-    stoi = np.mean(scores[test_path]['model']['STOI'])
-    snr = np.mean(scores[test_path]['model']['SNR'])
-    pesq_i = pesq - np.mean(scores[test_path]['ref']['PESQ'])
-    stoi_i = stoi - np.mean(scores[test_path]['ref']['STOI'])
-    snr_i = snr - np.mean(scores[test_path]['ref']['SNR'])
-    return np.array([pesq, stoi, snr, pesq_i, stoi_i, snr_i])
+    out = []
+    for test_path in test_paths:
+        pesq = np.mean(scores[test_path]['model']['PESQ'])
+        stoi = np.mean(scores[test_path]['model']['STOI'])
+        snr = np.mean(scores[test_path]['model']['SNR'])
+        pesq_i = pesq - np.mean(scores[test_path]['ref']['PESQ'])
+        stoi_i = stoi - np.mean(scores[test_path]['ref']['STOI'])
+        snr_i = snr - np.mean(scores[test_path]['ref']['SNR'])
+        out.append(np.array([pesq, stoi, snr, pesq_i, stoi_i, snr_i]))
+    return np.mean(out, axis=0)
 
 
-def get_crossval_dsets(dim, vals, i, j):
-    p = get_train_dset(**{dim: {vals[j]}})
-    p_ref = get_train_dset(**{dim: {v for v in vals if v != vals[j]}})
-    p_test = get_test_dset(**{dim: {vals[i]}})
-    return p, p_ref, p_test
-
-
-def gather_scores_single_mismatch():
-    scores_shape = (
-        len(archs),
-        len(dim_dict),
-        len(list(dim_dict.values())[0]),
-        len(list(dim_dict.values())[0]),
-        len(metrics)
-    )
-    out = np.empty(scores_shape)
-    out_ref = np.empty(scores_shape)
-    for i_arch, arch in enumerate(archs):
-        for i_dim, (dim, vals) in enumerate(dim_dict.items()):
-            matrix = np.empty((5, 5, 6))
-            matrix_ref = np.empty((5, 5, 6))
-            for i in range(5):
-                for j in range(5):
-                    p, p_ref, p_test = get_crossval_dsets(dim, vals, i, j)
-                    m = get_model(arch, p)
-                    m_ref = get_model(arch, p_ref)
-                    matrix[i, j, :] = get_scores(m, p_test)
-                    matrix_ref[i, j, :] = get_scores(m_ref, p_test)
-            out[i_arch, i_dim] = matrix
-            out_ref[i_arch, i_dim] = matrix_ref
-    return out, out_ref
-
-
-def gather_scores_double_mismatch():
-    dict_ = copy.deepcopy(dim_dict)
-    random.seed(0)
-    for dim in dict_.keys():
-        random.shuffle(dict_[dim])
-    scores_shape = (
-        len(archs),
-        len(dict_),
-        len(list(dict_.values())[0]),
-        len(list(dict_.values())[0]),
-        len(metrics)
-    )
-    out = np.empty(scores_shape)
-    out_ref = np.empty(scores_shape)
-    for i_dim, dims in enumerate(itertools.combinations(dict_.keys(), 2)):
-        for j, vals in enumerate(zip(dict_[dims[0]], dict_[dims[1]])):
-            kwargs = {dim: {val} for dim, val in zip(dims, vals)}
-            p = get_train_dset(**kwargs)
-            kwargs = {
-                dims[0]: {v for v in dict_[dims[0]] if v != vals[0]},
-                dims[1]: {v for v in dict_[dims[1]] if v != vals[1]},
-            }
-            p_ref = get_train_dset(**kwargs)
-            for i, vals_ in enumerate(zip(dict_[dims[0]], dict_[dims[1]])):
-                kwargs = {dim: {val} for dim, val in zip(dims, vals_)}
-                p_test = get_test_dset(**kwargs)
-                for i_arch, arch in enumerate(archs):
-                    m = get_model(arch, p)
-                    m_ref = get_model(arch, p_ref)
-                    out[i_arch, i_dim, i, j, :] = get_scores(m, p_test)
-                    out_ref[i_arch, i_dim, i, j, :] = get_scores(m_ref, p_test)
-    return out, out_ref
-
-
-def gather_scores_triple_mismatch():
-    dict_ = copy.deepcopy(dim_dict)
-    random.seed(0)
-    for dim in dict_.keys():
-        random.shuffle(dict_[dim])
-    random.seed(42)
-    for dim in dict_.keys():
-        random.shuffle(dict_[dim])
-    scores_shape = (
-        len(archs),
-        1,  # len(dict_),
-        len(list(dict_.values())[0]),
-        len(list(dict_.values())[0]),
-        len(metrics)
-    )
-    out = np.empty(scores_shape)
-    out_ref = np.empty(scores_shape)
-    for j, vals in enumerate(zip(*dict_.values())):
-        kwargs = {dim: {val} for dim, val in zip(dict_.keys(), vals)}
-        p = get_train_dset(**kwargs)
-        kwargs = {
-            dim: {v for v in dict_[dim] if v != val}
-            for dim, val in zip(dict_.keys(), vals)
-        }
-        p_ref = get_train_dset(**kwargs)
-        for i, vals_ in enumerate(zip(*dict_.values())):
-            kwargs = {dim: {val} for dim, val in zip(dict_.keys(), vals_)}
-            p_test = get_test_dset(**kwargs)
-            for i_arch, arch in enumerate(archs):
-                m = get_model(arch, p)
-                m_ref = get_model(arch, p_ref)
-                out[i_arch, 0, i, j, :] = get_scores(m, p_test)
-                out_ref[i_arch, 0, i, j, :] = get_scores(m_ref, p_test)
-    return out, out_ref
+def get_test_dsets(index):
+    test_paths = []
+    for i, j, k in itertools.product(*index):
+        test_path = get_test_dset(
+            speakers={databases[0]['databases'][i]},
+            noises={databases[1]['databases'][j]},
+            rooms={databases[2]['databases'][k]},
+        )
+        test_paths.append(test_path)
+    return test_paths
 
 
 def plot_bars(scores, scores_ref, which):
@@ -280,6 +268,11 @@ def plot_bars(scores, scores_ref, which):
         'double': 'upper center',
         'triple': (0.2275, 0.91),
     }[which]
+    i_dims = {
+        'single': [4, 5, 6],
+        'double': [1, 2, 3],
+        'triple': [0],
+    }[which]
 
     x = np.arange(len(archs)*2).reshape(len(archs), 2)
     x += bar_xspace*np.arange(len(archs)).reshape(-1, 1)
@@ -287,24 +280,17 @@ def plot_bars(scores, scores_ref, which):
 
     fig = plt.figure(figsize=figsize)
     outer_gs = gridspec.GridSpec(1, scores.shape[1], figure=fig)
-    for i_dim in range(scores.shape[1]):
-        dim, vals = list(dim_dict.items())[i_dim]
+    for i_gs, i_dim in enumerate(i_dims):
         inner_gs = gridspec.GridSpecFromSubplotSpec(
-            3, 2, subplot_spec=outer_gs[i_dim], hspace=0.05, wspace=0.05
+            3, 2, subplot_spec=outer_gs[i_gs], hspace=0.05, wspace=0.05
         )
         for i_ax, i_metric in enumerate(range(3, 6)):
             for i_config in range(2):
                 ax = fig.add_subplot(inner_gs[i_ax, i_config])
                 for i_arch in range(len(archs)):
 
-                    data = scores[i_arch, i_dim, :, :, i_metric]
-                    data_ref = scores_ref[i_arch, i_dim, :, :, i_metric]
-
-                    if i_config == 0:
-                        data = (data.sum(axis=0) - np.diag(data))/4
-                        data_ref = (data_ref.sum(axis=0) - np.diag(data_ref))/4
-                    else:
-                        data, data_ref = np.diag(data_ref), np.diag(data)
+                    data = scores[i_config, i_dim, :, i_arch, i_metric]
+                    data_ref = scores_ref[i_config, i_dim, :, i_arch, i_metric]
 
                     for is_ref, data_ in enumerate([data, data_ref]):
                         label = arch_labels[i_arch]
@@ -318,8 +304,7 @@ def plot_bars(scores, scores_ref, which):
 
                 ax.set_xticks([])
                 ax.set_xlim(xlim)
-                if i_dim == 0 and i_config == 0:
-                    # ax.set_ylabel(_m(metrics[i_metric]), fontsize='large')
+                if i_gs == 0 and i_config == 0:
                     ax.text(-0.275, 0.5, _m(metrics[i_metric]), rotation=90,
                             verticalalignment='center',
                             horizontalalignment='right',
@@ -330,7 +315,7 @@ def plot_bars(scores, scores_ref, which):
                 ax.grid(True, axis='y')
                 ax.set_yticks(yticks[i_ax])
                 ax.set_ylim(ylims[i_ax])
-                if i_dim != 0 or i_config != 0:
+                if i_gs != 0 or i_config != 0:
                     ax.set_yticklabels([])
 
         handles, labs = ax.get_legend_handles_labels()
@@ -355,25 +340,7 @@ def draw_gen_gap(ax, x, i_arch, data, data_ref, ylims):
     ax.annotate(G_e, (x+0.3, y+head_length*1.5), ha='center')
 
 
-def summary_table(single, single_ref, double, double_ref, triple, triple_ref):
-
-    def mean_diag(data, i_arch, i_metric):
-        mean = 0
-        for i_dim in range(data.shape[1]):
-            exp = data[i_arch, i_dim, :, :, i_metric]
-            diag = np.diag(exp)
-            mean += diag.mean()
-        mean /= data.shape[1]
-        return mean
-
-    def mean_anti_diag(data, i_arch, i_metric):
-        mean = 0
-        for i_dim in range(data.shape[1]):
-            exp = data[i_arch, i_dim, :, :, i_metric]
-            diag = np.diag(exp)
-            mean += (exp.sum() - diag.sum())/(exp.size - diag.size)
-        mean /= data.shape[1]
-        return mean
+def summary_table(scores):
 
     def print_cell(x, y):
         if x > y:
@@ -384,49 +351,43 @@ def summary_table(single, single_ref, double, double_ref, triple, triple_ref):
             y = rf'\textbf{{{y:.2f}}}'
         print(rf'& {x} & {y}', end=' ')
 
-    def print_row_match(data, data_ref, i_metric, header, indent):
-        for i in range(indent):
-            print('& ', end=' ')
-        print(rf'{header}', end=' ')
-        x = mean_diag(data, 0, i_metric)
-        y = mean_diag(data, 1, i_metric)
+    def print_row_match(i_metric):
+        print('& ', end=' ')
+        print('Match', end=' ')
+        x = scores[0, -1, :, 0, i_metric].mean()
+        y = scores[0, -1, :, 1, i_metric].mean()
         print_cell(x, y)
-        x = mean_anti_diag(data_ref, 0, i_metric)
-        y = mean_anti_diag(data_ref, 1, i_metric)
+        x = scores[1, -1, :, 0, i_metric].mean()
+        y = scores[1, -1, :, 1, i_metric].mean()
         print_cell(x, y)
         print(r'\\')
 
-    def print_row_mismatch(data, data_ref, i_metric, header, indent):
-        for i in range(indent):
-            print('& ', end=' ')
+    def print_row_mismatch(i_metric, header, i_dims):
+        print('& ', end=' ')
         print(rf'{header}', end=' ')
-        x = mean_anti_diag(data, 0, i_metric)
-        y = mean_anti_diag(data, 1, i_metric)
+        x = scores[0, i_dims, :, 0, i_metric].mean()
+        y = scores[0, i_dims, :, 1, i_metric].mean()
         print_cell(x, y)
-        x = mean_diag(data_ref, 0, i_metric)
-        y = mean_diag(data_ref, 1, i_metric)
+        x = scores[1, i_dims, :, 0, i_metric].mean()
+        y = scores[1, i_dims, :, 1, i_metric].mean()
         print_cell(x, y)
         print(r'\\')
 
     def print_block(i_metric):
-        print(r'\multirow{6}{*}{\rotatebox[origin=c]{90}', end='')
+        print(r'\multirow{4}{*}{\rotatebox[origin=c]{90}', end='')
         print(rf'{{{metrics[i_metric]}}}}}')
-        print(r'& \multirow{3}{*}{\rotatebox[origin=c]{90}{Match}}')
-        print_row_match(single, single_ref, i_metric, 'Single', 1)
-        print_row_match(double, double_ref, i_metric, 'Double', 2)
-        print_row_match(triple, triple_ref, i_metric, 'Triple', 2)
+        print_row_match(i_metric)
         print(r'\cline{2-7}')
-        print(r'& \multirow{3}{*}{\rotatebox[origin=c]{90}{Mism.}}')
-        print_row_mismatch(single, single_ref, i_metric, 'Single', 1)
-        print_row_mismatch(double, double_ref, i_metric, 'Double', 2)
-        print_row_mismatch(triple, triple_ref, i_metric, 'Triple', 2)
+        print_row_mismatch(i_metric, 'Single mism.', [4, 5, 6])
+        print_row_mismatch(i_metric, 'Double mism.', [1, 2, 3])
+        print_row_mismatch(i_metric, 'Triple mism.', [0])
 
     print(r'\begin{table}')
     print(r'\centering')
-    print(r'\begin{tabular}{ccccccc}')
+    print(r'\begin{tabular}{cccccc}')
     print(r'\hline \hline')
-    print(r'& & & \multicolumn{2}{c}{$N=1$} & \multicolumn{2}{c}{$N=4$} \\')
-    print(r'& & & FFNN & Conv-TasNet & FFNN & Conv-TasNet \\')
+    print(r'& & \multicolumn{2}{c}{$N=1$} & \multicolumn{2}{c}{$N=4$} \\')
+    print(r'& & FFNN & Conv-TasNet & FFNN & Conv-TasNet \\')
 
     print(r'\hline \hline')
     print_block(3)
@@ -441,32 +402,13 @@ def summary_table(single, single_ref, double, double_ref, triple, triple_ref):
     print(r'\label{tab:summary}')
     print(r'\end{table}')
 
-    def calc_mean(data, i_arch, i_metric):
-        mean = 0
-        for i_dim in range(data.shape[1]):
-            exp = data[i_arch, i_dim, :, :, i_metric+3]
-            diag = np.diag(exp)
-            mean += diag.mean()
-        mean /= data.shape[1]
-        return f'{mean:.2f}'
-
-
-def main():
-    scores_1, scores_ref_1 = gather_scores_single_mismatch()
-    # plot_bars(scores_1, scores_ref_1, 'single')
-    scores_2, scores_ref_2 = gather_scores_double_mismatch()
-    # plot_bars(scores_2, scores_ref_2, 'double')
-    scores_3, scores_ref_3 = gather_scores_triple_mismatch()
-    # plot_bars(scores_3, scores_ref_3, 'triple')
-    summary_table(
-        scores_1, scores_ref_1,
-        scores_2, scores_ref_2,
-        scores_3, scores_ref_3,
-    )
-    plt.show()
-
 
 if __name__ == '__main__':
     dset_init = DatasetInitializer(batch_mode=True)
     model_init = ModelInitializer(batch_mode=True)
-    main()
+    scores, ref_scores = gather_all_scores()
+    plot_bars(scores, ref_scores, 'single')
+    plot_bars(scores, ref_scores, 'double')
+    plot_bars(scores, ref_scores, 'triple')
+    summary_table(scores)
+    plt.show()
