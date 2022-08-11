d53f152fe364248eccb9a3e75116ba52e994a069
philgzl <services@philgzl.com>
2022-08-11 14:52:20 +0200

    Add model selection support in job scripts

--- jobs/train_model.sh
+++ jobs/train_model.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 OPTS=$(getopt \
-    --options fvq:c:m:w:g \
-    --longoptions force,verbose,queue:,cores:,memory:,walltime:,gpu32gb \
+    --options fvq:c:m:w:gM: \
+    --longoptions force,verbose,queue:,cores:,memory:,walltime:,gpu32gb,model: \
     -- "$@"
 )
 if [ $? -ne 0 ]; then exit 1; fi
@@ -15,6 +15,7 @@ CORES=1
 MEMORY=8
 WALLTIME=24
 GPU32GB=false
+MODEL=""
 
 while true; do
   case "$1" in
@@ -25,11 +26,12 @@ while true; do
     -m | --memory ) MEMORY="$2"; shift; shift ;;
     -w | --walltime ) WALLTIME="$2"; shift; shift ;;
     -g | --gpu32gb ) GPU32GB=true; shift ;;
+    -M | --model ) MODEL="$2"; shift; shift ;;
     -- ) shift; break ;;
   esac
 done
 
-if [ ${QUEUE} != "hpc" ] && [ ${QUEUE} != "gpuv100" ] && [ ${QUEUE} != "gpua100" ]
+if [ "${QUEUE}" != "hpc" ] && [ "${QUEUE}" != "gpuv100" ] && [ "${QUEUE}" != "gpua100" ]
 then
     echo "requested wrong queue: ${QUEUE}"
     exit 1
@@ -45,13 +47,19 @@ echo "#BSUB -R \"rusage[mem=${MEMORY}GB]\"" >> ${JOBFILE}
 echo "#BSUB -R \"span[hosts=1]\"" >> ${JOBFILE}
 echo "#BSUB -oo jobs/logs/%J.out" >> ${JOBFILE}
 echo "#BSUB -eo jobs/logs/%J.err" >> ${JOBFILE}
-if [ ${QUEUE} == "gpuv100" ] || [ ${QUEUE} == "gpua100" ]
+
+if [ "${QUEUE}" = "gpuv100" ] || [ "${QUEUE}" = "gpua100" ]
 then
     echo "#BSUB -gpu \"num=1:mode=exclusive_process\"" >> ${JOBFILE}
     if [ ${GPU32GB} = true ]
     then
         echo "#BSUB -R \"select[gpu32gb]\"" >> ${JOBFILE}
     fi
+fi
+
+if [ "${MODEL}" != "" ]
+then
+    echo "#BSUB -R \"select[model == ${MODEL}]\"" >> ${JOBFILE}
 fi
 
 echo "source venv/bin/activate" >> ${JOBFILE}
